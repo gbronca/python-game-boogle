c06511223a51676dc27a9d3e198dd1a327f272af
Giovanni <gbronca@gmail.com>
2018-12-04 21:37:12 +0000

    Initial commit

---
+++ test_boogle.py
@@ -0,0 +1,35 @@
+import unittest
+import boogle
+
+class TestBoogle(unittest.TestCase):
+    """
+    Our test suite for boogle solver
+    """
+
+    def test_can_create_an_empty_grid(self):
+        """
+        Test to see if we can create an empty grid
+        """
+        grid = boogle.make_grid(0,0)
+        self.assertEqual(len(grid),0)
+
+    def test_grid_size_is_width_times_height(self):
+        """
+        Test is to ensure that the total size of the grid
+        is equal to width * height
+        """
+
+        grid = boogle.make_grid(2,3)
+        self.assertEqual(len(grid),6)
+
+    def test_grid_coordinates(self):
+        """
+        Test to ensure that all of the coordinates
+        inside of the grid can be accessed.
+        """
+        grid = boogle.make_grid(2,2)
+        self.assertIn((0,0), grid)
+        self.assertIn((0,1), grid)
+        self.assertIn((1,0), grid)
+        self.assertIn((1,1), grid)
+        self.assertNotIn((2,2),grid)
